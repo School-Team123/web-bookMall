0de52dce612dc2d344f95b8c6f95b06dba8bc412
ZhouLian <947320114@qq.com>
2020-04-18 20:36:32 +0800

    管理员新增 修改 删除书籍

--- src/main/java/com/book/mall/controller/OrderController.java
+++ src/main/java/com/book/mall/controller/OrderController.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import static com.book.mall.controller.ManagerController.ipPath;
@@ -53,17 +54,52 @@ public class OrderController {
     @ResponseBody
     public Result getThisWeekHotBook()
     {
-        Result<List<Book>> result = new Result<>();
+        Result<List<HashMap>> result = new Result<>();
         result.setResultCode(200);
 
-        List a=new ArrayList();
+        List<HashMap> a=new ArrayList();
         int b[]=new int[5];
         for(int i=0;i<orderServiceImpl.selectWeekHotBook().size();i++)
         {
             b[i]=orderServiceImpl.selectWeekHotBook().get(i).getBookId();
             Book book=bookServiceImpl.selectBookById(b[i]);
             book.setImage_Path(ipPath+book.getImage_Path());
-            a.add(book);
+            String temp[]=book.getCategory().split("#");
+            List<String>temp2=new ArrayList<>();
+            for(String x  :  temp) {
+                switch (x) {
+                    case "1":
+                        temp2.add("计算机类");
+                        break;
+                    case "2":
+                        temp2.add("人文类");
+                        break;
+                    case "3":
+                        temp2.add("工程类");
+                        break;
+                    case "4":
+                        temp2.add("哲学类");
+                        break;
+                }
+            }
+            HashMap map=new HashMap();
+            map.put("bookId",book.getID_book());
+            map.put("bookName",book.getName_book());
+            map.put("language",book.getLanguage());
+            map.put("content",book.getContent());
+            map.put("category",temp2);
+            map.put("imagePath",book.getImage_Path());
+            map.put("author",book.getAuthor());
+            map.put("commend",book.getCommend());
+            map.put("bookNum",book.getBook_Num());
+            map.put("goodPrice",book.getGood_Price());
+            map.put("saleNum",book.getSale_Num());
+            map.put("publishName",book.getPublish_Name());
+            map.put("publishAddress",book.getPublish_address());
+            map.put("onSaleTime",book.getOn_Sale_Time());
+            map.put("price",book.getPrice());
+            a.add(map);
+
         }
 
         result.setMessage("获取本周热销书籍成功"+b);
@@ -75,17 +111,57 @@ public class OrderController {
     @ResponseBody
     public Result getThisMonthHotBook()
     {
-        Result<List<Order>> result = new Result<>();
+        Result<List<HashMap>> result = new Result<>();
         result.setResultCode(200);
-        List a=new ArrayList();
+        List<HashMap> a=new ArrayList<>();
         int b[]=new int[5];
         for(int i=0;i<orderServiceImpl.selectMonthHotBook().size();i++)
         {
             b[i]=orderServiceImpl.selectMonthHotBook().get(i).getBookId();
+
             Book book=bookServiceImpl.selectBookById(b[i]);
             book.setImage_Path(ipPath+book.getImage_Path());
-            a.add(book);
+            String temp[]=book.getCategory().split("#");
+            List<String>temp2=new ArrayList<>();
+            for(String x  :  temp) {
+                switch (x) {
+                    case "1":
+                        temp2.add("计算机类");
+                        break;
+                    case "2":
+                        temp2.add("人文类");
+                        break;
+                    case "3":
+                        temp2.add("工程类");
+                        break;
+                    case "4":
+                        temp2.add("哲学类");
+                        break;
+                }
+            }
+           HashMap map=new HashMap();
+            map.put("bookId",book.getID_book());
+            map.put("bookName",book.getName_book());
+            map.put("language",book.getLanguage());
+            map.put("content",book.getContent());
+            map.put("category",temp2);
+            map.put("imagePath",book.getImage_Path());
+            map.put("author",book.getAuthor());
+            map.put("commend",book.getCommend());
+            map.put("bookNum",book.getBook_Num());
+            map.put("goodPrice",book.getGood_Price());
+            map.put("saleNum",book.getSale_Num());
+            map.put("publishName",book.getPublish_Name());
+            map.put("publishAddress",book.getPublish_address());
+            map.put("onSaleTime",book.getOn_Sale_Time());
+            map.put("price",book.getPrice());
+            a.add(map);
+
         }
+
+
+
+
         result.setMessage("获取本月热销书籍成功"+b);
         result.setData(a);
         return result;
